2cab31bda82a7db407955a45e6f8be19ec720c03
Marko Kofol <marko.kofol@achtzig20.de>
2021-09-19 14:47:34 +0200

    Quizes architecture created.

---
+++ klepetko-frontend/src/app/Components/Quizes/quiz-english/quiz-english.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-english',
+  templateUrl: './quiz-english.component.html',
+  styleUrls: ['./quiz-english.component.scss']
+})
+export class QuizEnglishComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- klepetko-frontend/src/app/app.module.ts
+++ klepetko-frontend/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './Components/game/game.component';
+import { QuizEnglishComponent } from './components/quizes/quiz-english/quiz-english.component';
+import { QuizSlovenianComponent } from './components/quizes/quiz-slovenian/quiz-slovenian.component';
+import { QuizMathComponent } from './components/quizes/quiz-math/quiz-math.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    QuizEnglishComponent,
+    QuizSlovenianComponent,
+    QuizMathComponent
   ],
   imports: [
     BrowserModule,
